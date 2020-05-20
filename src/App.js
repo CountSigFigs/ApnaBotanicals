237ba182fca36659dbadf7c1e122253b7a1450cf
Brian Holt <bharrisonholt@gmail.com>
2020-05-19 19:34:13 -0700

    trying again

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import './App.css';
 import Main from './Components/MainComponent'
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import { ConfigureStore } from './Redux/configureStore';
 
@@ -9,11 +9,11 @@ export default function Routes(){
   const store = ConfigureStore();
     return (
       <Provider store={store}>
-        <BrowserRouter basename={process.env.PUBLIC_URL}>
+        <HashRouter basename='/'>
           <div id="root">
             <Main />
           </div>
-        </BrowserRouter>
+        </HashRouter>
       </Provider>
     )
   }
